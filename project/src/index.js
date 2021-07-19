fcd9cb4db512b1f6b1bc72dbaf1641cbe21fd2e4
xerveron <xerveron@gmail.com>
2021-07-19 18:02:08 +0300

    доделал props

--- project/src/index.js
+++ project/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
+const numberOffers = 312;
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App numberOffers={numberOffers}/>
   </React.StrictMode>,
   document.getElementById('root'));
