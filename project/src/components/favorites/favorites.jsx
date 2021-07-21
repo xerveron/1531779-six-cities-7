c75e6a08e1372a232068a4ee8dc3c5e1099b0b61
xerveron <xerveron@gmail.com>
2021-07-21 14:52:13 +0300

    Добавил шапку

--- project/src/components/favorites/favorites.jsx
+++ project/src/components/favorites/favorites.jsx
@@ -3,45 +3,83 @@ import Card from '../card/card';
 
 function Favorites() {
   return (
-    <main className='page__main page__main--favorites'>
-      <div className='page__favorites-container container'>
-        <section className='favorites'>
-          <h1 className='favorites__title'>Saved listing</h1>
-          <ul className='favorites__list'>
-            <li className='favorites__locations-items'>
-              <div className='favorites__locations locations locations--current'>
-                <div className='locations__item'>
-                  <a className='locations__item-link' href='#'>
-                    <span>Amsterdam</span>
+    <div className='page'>
+      <header className='header'>
+        <div className='container'>
+          <div className='header__wrapper'>
+            <div className='header__left'>
+              <a className='header__logo-link' href='main.html'>
+                <img
+                  className='header__logo'
+                  src='img/logo.svg'
+                  alt='6 cities logo'
+                  width='81'
+                  height='41'
+                />
+              </a>
+            </div>
+            <nav className='header__nav'>
+              <ul className='header__nav-list'>
+                <li className='header__nav-item user'>
+                  <a
+                    className='header__nav-link header__nav-link--profile'
+                    href='#'
+                  >
+                    <div className='header__avatar-wrapper user__avatar-wrapper'></div>
+                    <span className='header__user-name user__name'>
+                      Oliver.conner@gmail.com
+                    </span>
                   </a>
+                </li>
+                <li className='header__nav-item'>
+                  <a className='header__nav-link' href='#'>
+                    <span className='header__signout'>Sign out</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
+      <main className='page__main page__main--favorites'>
+        <div className='page__favorites-container container'>
+          <section className='favorites'>
+            <h1 className='favorites__title'>Saved listing</h1>
+            <ul className='favorites__list'>
+              <li className='favorites__locations-items'>
+                <div className='favorites__locations locations locations--current'>
+                  <div className='locations__item'>
+                    <a className='locations__item-link' href='#'>
+                      <span>Amsterdam</span>
+                    </a>
+                  </div>
                 </div>
-              </div>
-              <div className='favorites__places'>
-                <Card />
-                <Card />
-              </div>
-            </li>
+                <div className='favorites__places'>
+                  <Card />
+                  <Card />
+                </div>
+              </li>
 
-            <li className='favorites__locations-items'>
-              <div className='favorites__locations locations locations--current'>
-                <div className='locations__item'>
-                  <a className='locations__item-link' href='#'>
-                    <span>Cologne</span>
-                  </a>
+              <li className='favorites__locations-items'>
+                <div className='favorites__locations locations locations--current'>
+                  <div className='locations__item'>
+                    <a className='locations__item-link' href='#'>
+                      <span>Cologne</span>
+                    </a>
+                  </div>
                 </div>
-              </div>
-              <div className='favorites__places'>
-                <Card />
-              </div>
-            </li>
-          </ul>
-        </section>
-      </div>
-    </main>
+                <div className='favorites__places'>
+                  <Card />
+                </div>
+              </li>
+            </ul>
+          </section>
+        </div>
+      </main>
+    </div>
   );
 }
 
-Favorites.propTypes = {
-};
+Favorites.propTypes = {};
 
 export default Favorites;
