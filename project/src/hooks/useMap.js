6825ed0f3e210e13cbd6b8abdf7ebdafc7f2d228
xerveron <xerveron@gmail.com>
2021-07-30 19:55:43 +0300

    commit

--- project/src/hooks/useMap.js
+++ project/src/hooks/useMap.js
@@ -18,7 +18,7 @@ function useMap(mapRef, city) {
           'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
           {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-          }
+          },
         )
         .addTo(instance);
       setMap(instance);
